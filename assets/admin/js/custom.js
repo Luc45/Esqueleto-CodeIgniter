481c8fb5c19a84fb924b1d23429c8c8285a8a4c6
=Luc45 <lucasfbustamante@gmail.com>
2017-03-13 01:31:10 -0300

    Minor fix slugify do custom.js

--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -102,8 +102,8 @@ function slugify(e) {
 		// Passa uma string. Exemplo: slugify('este texto');
 
 		// remove accents, swap ñ for n, etc
- 		var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-  		var to   = "aaaaeeeeiiiioooouuuunc------";
+ 		var from = "àãáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  		var to   = "aaaaaeeeeiiiioooouuuunc------";
 		  for (var i=0, l=from.length ; i<l ; i++)
 		    e = e.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
 
@@ -120,8 +120,8 @@ function slugify(e) {
 		var Text = $(e).val();
 		
 		// remove accents, swap ñ for n, etc
- 		var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-  		var to   = "aaaaeeeeiiiioooouuuunc------";
+ 		var from = "àãáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  		var to   = "aaaaaeeeeiiiioooouuuunc------";
 		  for (var i=0, l=from.length ; i<l ; i++)
 		    Text = Text.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
 
